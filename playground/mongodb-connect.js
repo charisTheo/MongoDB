059406dbbc8745d668da63ada9fcabcb1944f985
charisTheo <haristh@hotmail.com>
2017-05-25 14:36:06 +0200

    Added delete feature

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -12,26 +12,26 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     }
     console.log('Connected to MongoDB Servers');
     
-    // db.collection('Todos').insertOne({
-    //     text: 'Something to do',
-    //     completed: false
-    // }, (err, result) => {
-    //     if (err) {
-    //         return console.log("Unable to insert todo", err);
-    //     }
-    //     console.log(JSON.stringify(result.ops, undefined, 2));
-    // });
-
-    db.collection('Users').insertOne({
-        name: 'Bullshit',
-        age: 23,
-        location: 'Roma, Italy'
-    }, (err ,result) => {
+    db.collection('Todos').insertOne({
+        text: 'Eat lunch',
+        completed: false
+    }, (err, result) => {
         if (err) {
-            return console.log('Unable to save user', err);
+            return console.log("Unable to insert todo", err);
         }
-        console.log(JSON.stringify(result.ops[0]._id.getTimestamp(), undefined, 2));
+        console.log(JSON.stringify(result.ops, undefined, 2));
     });
+
+    // db.collection('Users').insertOne({
+    //     name: 'Bullshit',
+    //     age: 23,
+    //     location: 'Roma, Italy'
+    // }, (err ,result) => {
+    //     if (err) {
+    //         return console.log('Unable to save user', err);
+    //     }
+    //     console.log(JSON.stringify(result.ops[0]._id.getTimestamp(), undefined, 2));
+    // });
 
     db.close();
 });
